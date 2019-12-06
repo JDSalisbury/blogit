b05567f623da078aea0e4c2c9ed5067bf3c50398
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-12-05 23:21:57 -0500

    Created a detail route for a Blog

--- src/modules/blogs.js
+++ src/modules/blogs.js
@@ -4,21 +4,35 @@ const actions = {
     const jsonResponse = require('../../public/blogs.json');
     commit('BLOGS_SUCCESS', jsonResponse);
   },
+  fetchBlog({ commit }, id) {
+    // eslint-disable-next-line global-require
+    const jsonResponse = require('../../public/blogs.json');
+    console.log(jsonResponse);
+    console.log(id);
+    // eslint-disable-next-line eqeqeq
+    const data = jsonResponse.filter(i => i.id == id);
+    console.log(data);
+    commit('GET_BLOG', data[0]);
+  },
 };
 
 const mutations = {
   // eslint-disable-next-line no-return-assign
   BLOGS_SUCCESS: (state, data) => (state.blogs = data),
+  // eslint-disable-next-line no-return-assign
+  GET_BLOG: (state, data) => (state.detail = data),
 };
 
 const getters = {
   blogState: state => state,
   getBlogs: state => state.blogs,
+  getDetail: state => state.detail,
 };
 
 const state = {
   blogs: '',
   tacos: 'They are good',
+  detail: '',
 };
 
 export default {
---
+++ src/views/Detail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="detail">
+    <Blog :item="getDetail" :key="getDetail.id" />
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import Blog from '../components/Blog.vue';
+
+export default {
+  name: 'Detail',
+  data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
+  components: {
+    Blog,
+  },
+  computed: mapGetters(['getDetail']),
+  methods: mapActions(['fetchBlog']),
+  created() {
+    this.fetchBlog(this.id);
+    console.log(this.getDetail);
+  },
+};
+</script>
