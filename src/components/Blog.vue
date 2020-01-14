e44c87561556ed2b16619df2ccb896336ee8cef8
JDSalisbury <Salisbury.jeffery@gmail.com>
2020-01-13 22:38:09 -0500

    Overlay created for image on phone set up.

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,30 +1,49 @@
 <template>
-<div>
-  <mq-layout mq='lg'>
-  <div class="main">
-    <img class='img-lg-screen' v-if="!detail" :src="item.picture" alt="test" />
+  <div>
+    <mq-layout mq="lg">
+      <div class="main">
+        <img
+          class="img-lg-screen"
+          v-if="!detail"
+          :src="item.picture"
+          alt="test"
+        />
 
-    <div class="juice">
-      <h1 v-if="detail">{{ item.title }}</h1>
-      <div v-else>
-        <router-link @click.native="scrollToTop" :to="'/' + item.id">
-          <h1>{{ item.title }}</h1>
-        </router-link>
+        <div class="juice">
+          <h1 v-if="detail">{{ item.title }}</h1>
+          <div v-else>
+            <router-link @click.native="scrollToTop" :to="'/' + item.id">
+              <h1>{{ item.title }}</h1>
+            </router-link>
+          </div>
+          <TagList :tags="item.tags" />
+          <img
+            class="img-lg-screen"
+            v-if="detail"
+            :src="item.picture"
+            alt="test"
+          />
+          <p>{{ detail ? item.body : vanish(item.body) }}</p>
+
+          <span>Created: {{ item.created_at }}</span>
+        </div>
       </div>
-      <TagList :tags="item.tags" />
-      <img class='img-lg-screen' v-if="detail" :src="item.picture" alt="test" />
-      <p>{{ detail ? item.body : vanish(item.body) }}</p>
-
-      <span>Created: {{ item.created_at }}</span>
-    </div>
-  </div>
-  </mq-layout>
-  <mq-layout mq='md'>
+    </mq-layout>
+    <mq-layout mq="md">
       <router-link @click.native="scrollToTop" :to="'/' + item.id">
-        <img class='img-sm-screen' v-if="!detail" :src="item.picture" alt="test" />
+        <h1>{{ item.title }}</h1>
+        <img
+          class="img-sm-screen"
+          v-if="!detail"
+          :src="item.picture"
+          alt="test"
+        />
       </router-link>
-  </mq-layout>
-</div>
+      <div class="tag-box">
+        <TagList class="overlay" :tags="item.tags" />
+      </div>
+    </mq-layout>
+  </div>
 </template>
 <script>
 import { vanish, scrollToTop } from '../lib/helperfunctions';
@@ -73,5 +92,19 @@ a {
 
 .img-sm-screen {
   margin: .25rem;
+}
+
+.overlay {
+  // background rgba(14, 14, 14, 0.81)
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.74), rgb(14, 14, 14));
+  width 300px
+  justify-content center
+  margin-top: -85px;
+  margin-bottom: 10px;
+}
+
+.tag-box {
+  display flex
+  justify-content center
 }
 </style>
